70c8f005e5cbd5612dbf506e1f07b2cfc47a0ca4
Ilya Skiba <kamaelkz@yandex.kz>
2019-11-26 11:04:25 +0600

    params

--- src/v1/config/main.php
+++ src/v1/config/main.php
@@ -57,48 +57,7 @@ $config = [
             'class' => 'kamaelkz\yii2admin\v1\controllers\DefaultController',
         ],
     ],
-    'params' => [
-        'yii2admin-sidebar' => [
-            'index' => [
-                'position' => 1,
-                'label' => Yii::t('yii2admin', 'Главная'),
-                'url' => ['/site/index'],
-                'icon' => 'icon-home4',
-                'active' => [
-                    'type' => 'ca',
-                    'rule' => ['site', 'index']
-                ],
-            ],
-            'uikit' => [
-                'position' => 1000,
-                'label' => 'UIkit',
-                'icon' => 'icon-stack',
-                'active' => [
-                    'type' => 'm',
-                    'rule' => 'uikit'
-                ],
-                'visible' => YII_DEBUG,
-                'children' => [
-                    [
-                        'label' => Yii::t('yii2admin', 'Виджеты'),
-                        'url' => ['/uikit/widgets'],
-                        'active' => [
-                            'type' => 'mc',
-                            'rule' => ['uikit', ['widgets']]
-                        ]
-                    ],
-                    [
-                        'label' => Yii::t('yii2admin', 'CRUD'),
-                        'url' => ['/uikit/crud'],
-                        'active' => [
-                            'type' => 'mc',
-                            'rule' => ['uikit', ['crud']],
-                        ]
-                    ],
-                ]
-            ],
-        ]
-    ]
+    'params' => require __DIR__ . '/params.php'
 ];
 
 if(YII_DEBUG) {
---
+++ src/v1/config/params.php
@@ -0,0 +1,44 @@
+<?php
+
+return [
+    'yii2admin-sidebar' => [
+        'index' => [
+            'position' => 1,
+            'label' => Yii::t('yii2admin', 'Главная'),
+            'url' => ['/site/index'],
+            'icon' => 'icon-home4',
+            'active' => [
+                'type' => 'ca',
+                'rule' => ['site', 'index']
+            ],
+        ],
+        'uikit' => [
+            'position' => 1000,
+            'label' => 'UIkit',
+            'icon' => 'icon-stack',
+            'active' => [
+                'type' => 'm',
+                'rule' => 'uikit'
+            ],
+            'visible' => YII_DEBUG,
+            'children' => [
+                [
+                    'label' => Yii::t('yii2admin', 'Виджеты'),
+                    'url' => ['/uikit/widgets'],
+                    'active' => [
+                        'type' => 'mc',
+                        'rule' => ['uikit', ['widgets']]
+                    ]
+                ],
+                [
+                    'label' => Yii::t('yii2admin', 'CRUD'),
+                    'url' => ['/uikit/crud'],
+                    'active' => [
+                        'type' => 'mc',
+                        'rule' => ['uikit', ['crud']],
+                    ]
+                ],
+            ]
+        ],
+    ]
+];
