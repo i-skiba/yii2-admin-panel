1eda0132b6ae94a218dcae7f3814c1c34b06f01a
i.skiba <ilya@mediabet.kz>
2020-04-06 08:55:24 +0000

    ++

--- src/v1/modules/hints/widgets/resources/scripts/yii2-hints.js
+++ src/v1/modules/hints/widgets/resources/scripts/yii2-hints.js
@@ -1,36 +1,41 @@
 $(document).ready(function () {
-    yii2admin.showPopover = false;
+    yii2admin.popover = {
+        class: 'yii2admin_hints',
+        elements: {},
+        is_show: false
+    };
+
     Yii2Admin.prototype.initAdminHints = function (json) {
+        yii2admin.popover.elements = $('.' + yii2admin.popover.class);
         _.each(json, function(value) {
             var hint = $('#yii2admin_hint_' + value.name);
             hint.attr('data-original-title', value.caption);
             hint.attr('data-content', value.value);
             hint.removeClass('d-none');
         });
+        
+        yii2admin.popover.elements.popover();
+        yii2admin.popover.elements.off('show.bs.popover');
+        yii2admin.popover.elements.on('show.bs.popover', function () {
+            yii2admin.popover.elements.popover('hide');
+            yii2admin.popover.is_show = true;
+        })
     };
 
-    var elementClass = 'yii2admin_hints';
-    var $elements = $('.' + elementClass);
-    $elements.popover();
     $(document).on('click', function(e) {
         var $target = $(e.target);
-        var is_hint = $target.hasClass(elementClass);
+        var is_hint = $target.hasClass(yii2admin.popover.class);
         if(
             ! is_hint
             && $target.closest('.popover').length === 0
-            && yii2admin.showPopover === true
+            && yii2admin.popover.is_show === true
         ) {
-            $elements.popover('hide');
-            yii2admin.showPopover = false;
+            yii2admin.popover.elements.popover('hide');
+            yii2admin.popover.is_show = false;
         }
 
         if(is_hint) {
             return false;
         }
     });
-
-    $elements.on('show.bs.popover', function () {
-        $elements.popover('hide');
-        yii2admin.showPopover = true;
-    })
 });
