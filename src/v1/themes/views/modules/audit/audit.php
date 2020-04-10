ab9df87f48e911969959ae2dc7d82140b0b78ce1
i.skiba <ilya@mediabet.kz>
2020-04-10 11:27:23 +0000

    refactor: audit index

--- src/v1/themes/views/modules/audit/audit.php
+++ src/v1/themes/views/modules/audit/audit.php
@@ -19,16 +19,21 @@ use kamaelkz\yii2admin\v1\modules\audit\themes\bundles\AuditBundle;
  * @var ActiveQuery $query
  * @var array $columns
  */
-
-$this->setTitle(Yii::t('yii2admin', 'Аудит'));
+?>
+<?php if (isset($originModel) && method_exists($originModel, 'toString')) :?>
+    <?php $this->setTitlePrefix(Yii::t('yii2admin', 'Аудит'));?>
+    <?php $this->setTitle($originModel->toString());?>
+<?php else:?>
+    <?php $this->setTitle(Yii::t('yii2admin', 'Аудит'));?>
+<?php endif?>
+<?php
 $this->pushBreadcrumbs(['label' => $model::label(), 'url' => ['index']]);
 $this->pushBreadcrumbs($this->title);
 if (isset($originModel)) {
     $this->viewHelper()->pushPageHeader(['update', 'id' => $originModel->id], Yii::t('yii2admin', 'Редактирование'),'icon-pencil6');
 }
+
 $this->viewHelper()->pushPageHeader(['index'], $model::label(),'icon-list');
-
-
 $params = isset($params) ? $params : Yii::$app->request->get();
 $extendColumns = isset($extendColumns) ? $extendColumns : [];
 
@@ -52,69 +57,96 @@ $auditDataProvider = new ActiveDataProvider([
 ?>
 
 <?php Pjax::begin(['id' => 'list-pjax']); ?>
-<?php if (isset($originModel) && method_exists($originModel, 'toString')): ?>
-    <h4><?= $originModel->toString(); ?></h4>
-<?php endif; ?>
-<div class="table-responsive">
-    <?= GridView::widget([
-        'layout' => '{summary}{pager}<br/>{items}{pager}',
-        'dataProvider' => $auditDataProvider,
-        'searchVisible' => true,
-        'searchParams' => [
-            'model' => $auditSearch
-        ],
-        'searchView' => '@yii2admin/themes/views/modules/audit/_search',
-        'columns' => [
-            [
-                'attribute' => 'user_id',
-                'value' => function ($data) {
-                    return $data->user_id;
-                },
-                'format' => 'raw',
+    <div class="table-responsive">
+        <?= GridView::widget([
+            'layout' => '{summary}{pager}<br/>{items}{pager}',
+            'dataProvider' => $auditDataProvider,
+            'searchVisible' => true,
+            'searchParams' => [
+                'model' => $auditSearch
             ],
-            [
-                'attribute' => 'username',
-                'label' => Yii::t('yii2admin', 'Имя пользователя'),
-                'value' => function ($data) {
-                    $user = User::findIdentity($data->user_id);
-                    return $user->toString();
-                },
-                'format' => 'raw',
-            ],
-            'action',
-//            'model',
-            'model_pk',
-            isset($models) ? [
-                'attribute' => 'field',
-                // TODO do something with this
-                'value' => function ($data) use ($models) {
-                    return $models[(int) $data->model_pk]->caption;
-                },
-                'format' => 'raw',
-            ] : 'field',
-            [
-                'label' => Yii::t('audit', 'Разница'),
-                'value' => function ($model) {
-                    $diff = $model->getDiffHtml();
-                    // TODO to view
-                    return "<div class='text-center'><span class=\"font-weight-semibold cursor-pointer\" data-toggle=\"collapse\" data-target=\"#collapse-text-{$model->id}\" aria-expanded=\"true\">" . Yii::t('yii2admin', 'Показать') . "</span></div><div class=\"collapse\" id=\"collapse-text-{$model->id}\" style=\"\">{$diff}</div>";
-                },
-                'format' => 'raw',
-            ],
-            'created_at',
-            [
-                'label' => Yii::t('audit', 'Действия'),
-                'value' => function ($model) {
-                    return Html::a('<i class="icon-rotate-ccw3"></i>', Url::to(['audit-rollback', 'id' => $model->id, 'model_pk' => $model->model_pk]), [
-                        'class' => 'admin-action btn btn-icon bg-success-400',
-                        'data-pjax-id' => 'list-pjax',
-                        'data-pjax-url' => Url::current([], true),
-                        'data-swal' => Yii::t('yii2admin' , 'Вернуть старое значение')
-                    ]);
-                },
-                'format' => 'raw',
-            ],
-        ]
-    ]); ?>
-</div>
+            'searchView' => '@yii2admin/themes/views/modules/audit/_search',
+            'columns' => [
+                [
+                    'attribute' => 'user_id',
+                    'label' => Yii::t('yii2admin', 'Пользователь'),
+                    'value' => function ($data) {
+                        $user_id = $data->user_id;
+                        $user = User::findIdentity($user_id);
+                        if(! $user) {
+                            return null;
+                        }
+
+                        return Html::tag(
+                            'a',
+                            $user->toString(),
+                            [
+                                'class' => 'badge bg-primary',
+                                'target' => '_blank',
+                                'href' => Url::to(['user/user/view', 'id' => $user_id])
+                            ]
+                        );
+                    },
+                    'format' => 'raw',
+                ],
+                'action',
+//                'model_pk',
+                'created_at',
+                isset($models) ? [
+                    'attribute' => 'field',
+                    // TODO do something with this
+                    'value' => function ($data) use ($models) {
+                        return $models[(int) $data->model_pk]->caption;
+                    },
+                    'format' => 'raw',
+
+                ] : 'field',
+                [
+                    'label' => Yii::t('yii2admin', 'Разница'),
+                    'value' => function ($model) {
+                        $diff = $model->getDiffHtml();
+                        // TODO to view
+                        $header = Yii::t('yii2admin', 'Показать');
+                        return <<<HTML
+                            <div class='text-center'>
+                            <span class="font-weight-semibold cursor-pointer" data-toggle="collapse" data-target="#collapse-text-{$model->id}" aria-expanded="true">
+                                {$header}
+                            </span>
+                            </div>
+                            <div class="collapse" id="collapse-text-{$model->id}" style="">
+                                {$diff}
+                            </div>
+HTML;
+
+                    },
+                    'headerOptions' => [
+                        'style' => 'width:35%',
+                        'class' => 'text-center'
+                    ],
+                    'contentOptions' => [
+                        'class' => 'text-center'
+                    ],
+                    'format' => 'raw',
+                ],
+                [
+                    'label' => Yii::t('yii2admin', 'Действия'),
+                    'value' => function ($model) {
+                        return Html::a('<i class="icon-arrow-left13"></i> ' . Yii::t('yii2admin', 'Востановить'), Url::to(['audit-rollback', 'id' => $model->id, 'model_pk' => $model->model_pk]), [
+                            'class' => 'admin-action list-icons-item btn bg-success',
+                            'data-pjax-id' => 'list-pjax',
+                            'data-pjax-url' => Url::current([], true),
+                            'data-swal' => Yii::t('yii2admin' , 'Вернуть старое значение')
+                        ]);
+                    },
+                    'headerOptions' => [
+                        'class' => 'text-center'
+                    ],
+                    'contentOptions' => [
+                        'class' => 'text-center'
+                    ],
+                    'format' => 'raw',
+                ],
+            ]
+        ]); ?>
+    </div>
 <?php Pjax::end(); ?>
