515b65c61bc3c80867ad18142599000984c88ad4
i.skiba <ilya@mediabet.kz>
2020-11-25 06:50:17 +0000

    fix: fornts

--- src/v1/themes/bundles/StylesBundle.php
+++ src/v1/themes/bundles/StylesBundle.php
@@ -14,10 +14,11 @@ class StylesBundle extends Bundle
     public $sourcePath = '@yii2admin/themes/resources';
 
     public $css = [
-        'limitless/assets/css/roboto-font.css',
+//        'limitless/assets/css/roboto-font.css',
         'limitless/global_assets/css/icons/icomoon/styles.min.css',
         'limitless/global_assets/css/icons/fontawesome/styles.min.css',
-        YII_DEBUG ? 'limitless/assets/css/bootstrap.css' : 'limitless/assets/css/bootstrap.min.css',
+//        YII_DEBUG ? 'limitless/assets/css/bootstrap.css' : 'limitless/assets/css/bootstrap.min.css',
+        'limitless/assets/css/bootstrap.css',
         YII_DEBUG ? 'limitless/assets/css/bootstrap_limitless.css' : 'limitless/assets/css/bootstrap_limitless.min.css',
         YII_DEBUG ? 'limitless/assets/css/layout.min.css' : 'limitless/assets/css/layout.css',
         YII_DEBUG ? 'limitless/assets/css/components.min.css' : 'limitless/assets/css/components.css',
